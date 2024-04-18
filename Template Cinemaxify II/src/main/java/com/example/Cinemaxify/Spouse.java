f09a993ea1de19ce17d0ae627e2e32a63716b8c7
Suraj karki <singhkarki160@gmail.com>
2024-04-18 14:57:38 +0530

    Initial commit

---
+++ Template Cinemaxify II/src/main/java/com/example/Cinemaxify/Spouse.java
@@ -0,0 +1,50 @@
+package com.example.Cinemaxify;
+
+public class Spouse implements User{
+
+    private String name;
+    private String memberName = "spouse";
+    private int age;
+    private Long contact;
+    private String address;
+    /**
+     1. Add attribute private Plan plan.
+
+     2. Create a parameterized constructor as shown below:
+
+        public Spouse(Plan plan)    {
+
+        It accepts a Plan parameter and assigns
+        it to the class plan attribute.
+
+            }
+
+     **/
+     private Plan plan;
+     public Spouse(Plan plan) {
+         this.plan = plan;
+     }
+
+    @Override
+    public void setUserDetails(String name, int age, Long contact, String address) {
+        this.name = name;
+        this.age = age;
+        this.contact = contact;
+        this.address = address;
+    }
+
+    @Override
+    public void getUserDetails() {
+        System.out.println("Hello " + name + " you have entered the following details:");
+        System.out.println("age: " +age + "\ncontact: " + contact + "\naddress: " +address);
+        System.out.println("You have selected a " + plan.getPlanName() + " plan for " + memberName);
+    }
+
+    @Override
+    public Plan getUserPlan() {
+        return plan;
+    }
+
+    // 3. Override the Plan getUserPlan(): It returns the plan attribute
+
+}
